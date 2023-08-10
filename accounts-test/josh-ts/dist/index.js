f38f40150ba1d5a9c1271d2942464c8440baee6e
daoistcoder <deservekindness@gmail.com>
2023-08-10 18:24:14 +0800

    UPDATE: account-test dir

--- accounts-test/josh-ts/dist/index.js
+++ accounts-test/josh-ts/dist/index.js
@@ -1,11 +1,7 @@
 import fs from 'fs';
-
 console.log(123);
-
-const addNumber = (a: number, b: number) => {
+const addNumber = (a, b) => {
     return a + b;
 };
-
-fs.writeFileSync("test.txt", "test")
-
+fs.writeFileSync("test.txt", "test");
 console.log("sum is:", addNumber(1, 2));
